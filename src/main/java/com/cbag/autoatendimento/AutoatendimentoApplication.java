5aa8d57497a86eaceaece9e0119727ed3f8da42a
Cauã Bassin <cauabassin@id.uff.br>
2026-07-24 15:58:23 -0300

    inicial

---
+++ src/main/java/com/cbag/autoatendimento/AutoatendimentoApplication.java
@@ -0,0 +1,13 @@
+package com.cbag.autoatendimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoatendimentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoatendimentoApplication.class, args);
+    }
+
+}
