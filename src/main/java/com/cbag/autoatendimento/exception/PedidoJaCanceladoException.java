44f90d6ee35a912414b4fefa0404eb9ae0c3f4ab
flafmg <flaafmg@gmail.com>
2026-08-29 10:04:20 -0300

    esqueci

---
+++ src/main/java/com/cbag/autoatendimento/exception/PedidoJaCanceladoException.java
@@ -0,0 +1,7 @@
+package com.cbag.autoatendimento.exception;
+
+public class PedidoJaCanceladoException extends Exception {
+    public PedidoJaCanceladoException(String message) {
+        super(message);
+    }
+}
