6685f7536bb42f9eeccba19496a7cc737487a756
csbassin <cauabassin@id.uff.br>
2026-09-04 11:40:31 -0300

    tinha botado o nome errado na variável no repository. Meu autocomplete não tá funcionando. ops

--- src/main/java/com/cbag/autoatendimento/repo/PedidoRepository.java
+++ src/main/java/com/cbag/autoatendimento/repo/PedidoRepository.java
@@ -3,6 +3,7 @@ package com.cbag.autoatendimento.repo;
 import com.cbag.autoatendimento.model.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -11,6 +12,6 @@ import java.util.List;
 @Repository
 public interface PedidoRepository<T extends Pedido> extends JpaRepository<T,Long> {
     // todo testar
-    @Query("select pedido from Pedido pedido where cast(pedido.dateTime as DATE) = cast(dateTime as DATE)")
-    List<Pedido> findWhereCreationDateIsEqual(LocalDateTime dateTime);
+    @Query("select pedido from Pedido pedido where cast(pedido.timestamp as DATE) = cast(:dateTime as DATE)")
+    List<Pedido> findWhereCreationDateIsEqual(@Param("dateTime")LocalDateTime dateTime);
 }
